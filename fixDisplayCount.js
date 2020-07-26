3093c9ab57a9a82c86236fcf48cf9674c8b178f2
Roi Rosenthal <roir@wix.com>
2020-07-26 20:00:27 +0300

    make public

--- fixDisplayCount.js
+++ fixDisplayCount.js
@@ -1,15 +1,19 @@
 try {
+  fixed=0
   menus=documentServices.menu.getAll()
   menus.map(menu=>
     menu.items.map(mi=>{
       if (mi.displayCount===0) {
         mi.displayCount=undefined;
+        fixed++
         documentServices.menu.updateItem(menu.id,mi.id,mi)
       }
     })
     )
     window.autopilotJsonp({
+      fixed
   })
 } catch (e) {
   window.autopilotJsonp(null, e)
 }
+
