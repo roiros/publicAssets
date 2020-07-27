8f6f30818e9d9a4ef9ba1bb7c4571f735d462cf3
Roi Rosenthal <roir@wix.com>
2020-07-27 20:26:17 +0300

    refactor to flatMap

--- fixDisplayCount.js
+++ fixDisplayCount.js
@@ -1,34 +1,25 @@
 try {
   let fixedCount = 0, positiveCount = 0, undefinedCount = 0
-  const isMultilingualEnabled=documentServices.language.multilingual.isEnabled()
+  const isMultilingualEnabled = documentServices.language.multilingual.isEnabled()
 
   const menus = documentServices.menu.getAll()
-  menus.forEach(menu => {
-    if (menu.items) {
-      const itemsStack = []
-      itemsStack.push(...menu.items)
 
-      while (itemsStack.length) {
-        const menuItem = itemsStack.pop()
-        if (menuItem.items) {
-          itemsStack.push(...menuItem.items)
-        }
+  const getItems = (obj) => { if (obj.items) { return [obj, ...obj.items.map(getItems)] } else return [obj] }
+  const itemsByMenu = menus.map(menu => ({ menuId: menu.id, items: _.flatMapDeep(menu.items, getItems) }))
 
-        if (menuItem.displayCount === 0) {
-          menuItem.displayCount = undefined;
-          fixedCount++
-          documentServices.menu.updateItem(menu.id, menuItem.id, menuItem)
-        } else if (menuItem.displayCount > 0) {
-          positiveCount++
-        } else if (menuItem.displayCount === undefined) {
-          undefinedCount++
-        }
+  itemsByMenu.forEach(({ menuId, items }) => {
+    items.forEach(menuItem => {
+      if (menuItem.displayCount === 0) {
+        menuItem.displayCount = undefined;
+        fixedCount++
+        documentServices.menu.updateItem(menuId, menuItem.id, menuItem)
+      } else if (menuItem.displayCount > 0) {
+        positiveCount++
+      } else if (menuItem.displayCount === undefined) {
+        undefinedCount++
       }
-    }
+    })
   })
-  return {fixedCount,
-    positiveCount,
-    undefinedCount}
 
   window.autopilotJsonp({
     isMultilingualEnabled,
